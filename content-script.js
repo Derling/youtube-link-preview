ae20d1c55f5c5ef80ad7c15451412ece49fd500f
Derling Batista Diaz <derling0704@gmail.com>
2024-02-22 18:55:01 -0500

    use different regex + support same vid in different message

--- content-script.js
+++ content-script.js
@@ -1,12 +1,13 @@
-PARAM_START_CHAR = "?"
-
 // youtube constants
-YOUTUBE_URL_REGEX = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/g; // https://regexr.com/3dj5t
-VIDEO_ID_PARAM = "v";
+YOUTUBE_URL_REGEX = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/; // https://stackoverflow.com/questions/3452546/how-do-i-get-the-youtube-video-id-from-a-url
+YOUTUBE_ID_REGEX_INDEX = 7;
+YOUTUBE_ID_LENGTH = 11;
 
 
 // 7tv constants
 USER_MESSAGE_SPAN_CLASS = "seventv-user-message";
+USER_MESSAGE_ID_ATTRIBUTE = "msg-id";
+PREVIEW_ID_PREFIX = "twitch-yt";
 
 function createIframe(videoId, elementId) {
     // allow is part of the default html from video pages, just copying it over to have similar behavior
@@ -21,9 +22,11 @@ function createIframe(videoId, elementId) {
 }
 
 
-function getVideoId(url) {
-    const urlParams = new URLSearchParams(url.split(PARAM_START_CHAR)[1]);
-    return urlParams.get(VIDEO_ID_PARAM);
+function getVideoId(match) {
+    if(!(match && match[YOUTUBE_ID_REGEX_INDEX].length === YOUTUBE_ID_LENGTH)) {
+        return;
+    }
+    return match[YOUTUBE_ID_REGEX_INDEX];
 }
 
 function handleMouseOver(event) {
@@ -37,8 +40,10 @@ function handleMouseOver(event) {
         return;
     }
 
-    const videoId = getVideoId(match[0]);
-    const elementId = `twitch-yt-${videoId}`;
+    const videoId = getVideoId(match);
+    const msgId = element.getAttribute(USER_MESSAGE_ID_ATTRIBUTE);
+    const elementId = `${PREVIEW_ID_PREFIX}-${videoId}-${msgId}`;
+
     if (document.getElementById(elementId) !== null) {
         return;
     }
